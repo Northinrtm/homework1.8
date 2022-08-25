edbd6cc60f751419dbda9c5b7feaf87e71bc7a96
northinrtm <artsdurden@gmail.com>
2022-08-25 16:15:02 +0300

    исправил 3 задание

--- src/Task3.java
+++ src/Task3.java
@@ -1,14 +1,15 @@
 public class Task3 {
-    static void checkDelivery(int deliveryDistance) {
-        if (deliveryDistance <= 20) System.out.println("Потребуется дней: " + 1);
-        if ((20 < deliveryDistance) && (deliveryDistance <= 60)) System.out.println("Потребуется дней: " + 2);
-        if ((60 < deliveryDistance) && (deliveryDistance <= 100)) System.out.println("Потребуется дней: " + 3);
+    private static int checkDelivery(int deliveryDistance) {
+        int a = 1;
+        if (20 < deliveryDistance) a += 1;
+        if (60 < deliveryDistance) a += 1;
+        return a;
     }
 
     public static void main(String[] args) {
         int deliveryDistance = 95;
-        checkDelivery(1);
-        checkDelivery(21);
-        checkDelivery(deliveryDistance);
+        System.out.println("Потребуется дней: " + checkDelivery(1));
+        System.out.println("Потребуется дней: " + checkDelivery(21));
+        System.out.println("Потребуется дней: " + checkDelivery(deliveryDistance));
     }
 }
